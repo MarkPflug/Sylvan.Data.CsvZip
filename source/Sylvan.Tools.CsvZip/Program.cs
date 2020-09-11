b1505b86c4b11d45d911059fdc15e90917fce96b
MarkPflug <mark.pflug@live.com>
2020-09-11 13:36:02 -0700

    csvz creation tool

--- source/Sylvan.Tools.CsvZip/Program.cs
+++ source/Sylvan.Tools.CsvZip/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Sylvan.Data;
+using Sylvan.Data.Csv;
 using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
 
 namespace Sylvan.Tools.CsvZip
 {
@@ -7,7 +10,51 @@ namespace Sylvan.Tools.CsvZip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rootCommand = new RootCommand
+            {
+                new Option<string>(
+                    "--dir",
+                    getDefaultValue: () => ".",
+                    description: "The directory to package into a CSVZ"
+                ),
+                new Option<string>(
+                    "--file",
+                    description: "The name of the target CSVZ file"                 
+                )
+            };
+
+            rootCommand.Description = "Packages a CSVZ file.";
+            rootCommand.Handler = CommandHandler.Create<string, string>(Package);
+            rootCommand.Invoke(args);
+        }
+
+        static void Package(string dir, string file)
+        {
+            dir ??= ".";
+
+            if(file == null)
+            {
+                var d = Path.GetDirectoryName(Path.GetFullPath(dir));
+                var f = Path.GetFileName(d);
+                file = f + ".csvz";
+            }
+
+            using var csvz = CsvZipPackage.Create(file);
+
+            foreach (var csv in Directory.EnumerateFiles(dir, "*.csv", SearchOption.TopDirectoryOnly)) {
+                var data = CsvDataReader.Create(csv);
+                var analyzer = new SchemaAnalyzer();
+                var result = analyzer.Analyze(data);
+
+                var schema = result.GetSchema();
+                var csvSchema = new CsvSchema(schema);
+
+                var csvOpts = new CsvDataReaderOptions { Schema = csvSchema };
+
+                data = CsvDataReader.Create(csv, csvOpts);
+                var entry = csvz.CreateEntry(Path.GetFileName(csv));
+                entry.WriteData(data);
+            }
         }
     }
 }
