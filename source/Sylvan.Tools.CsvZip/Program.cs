e218612bd188faf0f2561d53efb0f3c9cb63180e
MarkPflug <mark.pflug@live.com>
2020-09-11 14:14:23 -0700

    Improve output. Fix folder name bug.

--- source/Sylvan.Tools.CsvZip/Program.cs
+++ source/Sylvan.Tools.CsvZip/Program.cs
@@ -1,8 +1,13 @@
 ﻿using Sylvan.Data;
 using Sylvan.Data.Csv;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace Sylvan.Tools.CsvZip
 {
@@ -28,33 +33,67 @@ namespace Sylvan.Tools.CsvZip
             rootCommand.Invoke(args);
         }
 
-        static void Package(string dir, string file)
+        static void Error(string msg) {
+            var c = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine(msg);
+            Console.ForegroundColor = c;
+        }
+
+        static IEnumerable<string> GetCsvs(string dir)
+        {
+            return Directory.EnumerateFiles(dir, "*.csv", SearchOption.TopDirectoryOnly);
+        }
+
+        static int Package(string dir, string file)
         {
             dir ??= ".";
 
-            if(file == null)
+            if (!Directory.Exists(dir))
             {
-                var d = Path.GetDirectoryName(Path.GetFullPath(dir));
+                Error($"Directory '{dir}' doesn't exist.");
+                return 1;
+            }
+
+            if (GetCsvs(dir).Any() == false)
+            {
+                Error($"Directory '{dir}' doesn't contain any csvs.");
+                return 2;
+            }
+
+            if (file == null)
+            {
+                var d = Path.GetFullPath(dir);
                 var f = Path.GetFileName(d);
                 file = f + ".csvz";
             }
 
+            Console.WriteLine("Creating: " + file);
             using var csvz = CsvZipPackage.Create(file);
+            Stopwatch sw;
 
             foreach (var csv in Directory.EnumerateFiles(dir, "*.csv", SearchOption.TopDirectoryOnly)) {
+                Console.WriteLine($"Processing {Path.GetFileName(csv)}: ");
                 var data = CsvDataReader.Create(csv);
                 var analyzer = new SchemaAnalyzer();
+                Console.Write($"  Analyzing. ");
+                sw = Stopwatch.StartNew();
                 var result = analyzer.Analyze(data);
+                Console.WriteLine(sw.Elapsed.ToString());
 
                 var schema = result.GetSchema();
                 var csvSchema = new CsvSchema(schema);
 
                 var csvOpts = new CsvDataReaderOptions { Schema = csvSchema };
-
+                Console.Write($"  Writing.   ");
                 data = CsvDataReader.Create(csv, csvOpts);
+                sw = Stopwatch.StartNew();
                 var entry = csvz.CreateEntry(Path.GetFileName(csv));
                 entry.WriteData(data);
+                Console.WriteLine(sw.Elapsed.ToString());
             }
+            Console.WriteLine("Done.");
+            return 0;
         }
     }
 }
